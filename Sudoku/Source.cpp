ec07c2536af44c2b355a2bc0028643649b5ccd13
Rok Oblak <oblak2003@gmail.com>
2022-10-15 17:26:11 +0200

    a

--- Sudoku/Source.cpp
+++ Sudoku/Source.cpp
@@ -7,13 +7,14 @@
 #include "Game.h"
 #include "Manager.h"
 
-bool init();
+bool Init();
+void Quit();
 
 int main(int argc, char** args)
 {
 	srand(time(nullptr));
 
-	if (!Manager::InitialiseWindow(1280, 720, "Sudoku") || !init())
+	if (!Manager::InitialiseWindow(1280, 720, "Sudoku") || !Init())
 	{
 		return 1;
 	}
@@ -29,14 +30,19 @@ int main(int argc, char** args)
 
 	GameObj.reset();
 
-	IMG_Quit();
-	TTF_Quit();
-	SDL_Quit();
+	Quit();
 
 	return 0;
 }
 
-bool init()
+bool Init()
 {
 	return !(SDL_Init(SDL_INIT_EVERYTHING) != 0 || IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG || TTF_Init() != 0);
+}
+
+void Quit()
+{
+	IMG_Quit();
+	TTF_Quit();
+	SDL_Quit();
 }
